4666d1efcb0d03fd70e11d6b5f80013f78652961
huan98 <847693739@qq.com>
2020-01-19 18:00:09 +0800

    用户微博列表

--- database/factories/StatusFactory.php
+++ database/factories/StatusFactory.php
@@ -5,8 +5,11 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Models\Status::class, function (Faker $faker) {
+    $date_time = $faker->date . ' ' . $faker->time;
     return [
-        //
+        'content' => $faker->text(),
+        'created_at' => $date_time,
+        'updated_at' => $date_time,
     ];
 });
